9ae8fc40afed79e61fa795453e618876e55436ce
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-02-18 01:22:31 +0100

    Add basic site with form and sessions

---
+++ hello.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+app = Flask(__name__)
+
+app.secret_key = "\xef\xf2\x14b\xdb}\x1bF\xfbw\xc25\xd2GJ\x86"
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+
+@app.route("/greeting/<name>")
+def hello(name):
+	return render_template("hello.html", t_name=name)
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+	"""Use get to get any of the variables"""
+	if request.method == "POST":
+		first_name = request.values.get("first_name")
+		last_name = request.values.get("last_name")
+		session["first_name"] = first_name
+		return redirect(url_for("registered"))
+	return render_template("form.html")
+
+@app.route("/thank_you")
+def registered():
+	first_name = session.get("first_name")
+	return f"Thank you, {first_name}!"
